fd05b54805cc26d11ad93632a126f9e294306817
Michał <michalplocieniak@gmail.com>
2020-08-06 21:45:24 +0200

    gitignore update

---
+++ src/ImplementatorServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ritenn\Implementator;
+
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class ImplementatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
